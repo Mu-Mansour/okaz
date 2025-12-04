8e2a4243a555f4b975d4fdfc0279619f1a58faf0
Mansour <v14mmotaleb11@gmail.com>
2025-12-05 01:58:19 +0200

    add signUp Form Schema and action

--- lib/actions/userActions.ts
+++ lib/actions/userActions.ts
@@ -1,7 +1,9 @@
 "use server";
 import { signIn, signOut } from "@/auth";
-import { signInFormSchema } from "../validator";
+import { signInFormSchema, signUpFormSchema } from "../validator";
 import { isRedirectError } from "next/dist/client/components/redirect-error";
+import { hashSync } from "bcrypt-ts";
+import { prisma } from "@/db/prisma";
 export async function signInWithCredentials(
   prevState: unknown,
   formData: FormData
@@ -25,4 +27,42 @@ export async function signInWithCredentials(
 }
 export async function signOutUser() {
   await signOut();
+}
+export async function signUp(prevState: unknown, formData: FormData) {
+  try {
+    const user = signUpFormSchema.parse({
+      name: formData.get("name"),
+      email: formData.get("email"),
+      confirmPassword: formData.get("confirmPassword"),
+      password: formData.get("password"),
+    });
+
+    const plainPassword = user.password;
+
+    user.password = hashSync(user.password, 10);
+
+    await prisma.user.create({
+      data: {
+        name: user.name,
+        email: user.email,
+        password: user.password,
+      },
+    });
+
+    await signIn("credentials", {
+      email: user.email,
+      password: plainPassword,
+    });
+
+    return { success: true, message: "User created successfully" };
+  } catch (error) {
+    if (isRedirectError(error)) {
+      throw error;
+    }
+
+    return {
+      success: false,
+      message: "Something went wrong",
+    };
+  }
 }
