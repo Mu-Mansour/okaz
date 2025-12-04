8fb577eb3d177dbf7a290ef5cfebce5d16f437f2
Mansour <v14mmotaleb11@gmail.com>
2025-12-04 03:21:04 +0200

    add backend prisma configuration  and first table Product setup with utility functions and its seeder as well

---
+++ db/prisma.ts
@@ -0,0 +1,27 @@
+import { neonConfig } from "@neondatabase/serverless";
+import { PrismaNeon } from "@prisma/adapter-neon";
+import ws from "ws";
+import { PrismaClient } from "../lib/generated/prisma/client";
+
+neonConfig.webSocketConstructor = ws;
+
+const connectionString = `${process.env.DATABASE_URL}`;
+
+const adapter = new PrismaNeon({ connectionString });
+
+export const prisma = new PrismaClient({ adapter }).$extends({
+  result: {
+    product: {
+      price: {
+        compute(product) {
+          return product.price.toString();
+        },
+      },
+      rating: {
+        compute(product) {
+          return product.rating.toString();
+        },
+      },
+    },
+  },
+});
