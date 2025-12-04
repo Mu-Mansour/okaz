b20599553cfece0c28fe5051977868e773e1bbfd
Mansour <v14mmotaleb11@gmail.com>
2025-12-04 03:16:57 +0200

    add initial libraries setup and assets with base theme beside loading and not-found

---
+++ lib/constants/index.ts
@@ -0,0 +1,6 @@
+export const APP_NAME = "Okaz";
+export const APP_DESCRIPTION =
+  "Your one-stop shop for incredible deals on everything you love.";
+export const SERVER_URL = process.env.NEXT_PUBLIC_SERVER_URL;
+export const LATEST_PRODUCTS_LIMIT =
+  Number(process.env.LATEST_PRODUCTS_LIMIT) || 6;
