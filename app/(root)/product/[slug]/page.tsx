e4d5e3b42d1709a671321eafcc30c891305c2205
Mansour <v14mmotaleb11@gmail.com>
2025-12-15 09:00:24 +0200

    fix ui price badge

--- app/(root)/product/[slug]/page.tsx
+++ app/(root)/product/[slug]/page.tsx
@@ -46,7 +46,7 @@ const ProductDetailsPage = async (props: {
               <div className='flex flex-col gap-3 sm:flex-row sm:items-center'>
                 <ProductPrice
                   value={Number(product.price)}
-                  className='w-24 rounded-full bg-green-100 text-green-700 px-5 py-2'
+                  className='w-fit rounded-full bg-green-100 text-green-700 px-5 py-2'
                 />
               </div>
             </div>
